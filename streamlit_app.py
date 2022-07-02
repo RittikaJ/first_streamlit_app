7280f5d1810d15884557adf55c6bc9c5444bc93b
RittikaJ <5206.rittika@gmail.com>
2022-07-03 01:21:40 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import streamlit
 
 streamlit.title('Diner')
 
-streamlit.header('Menu')
-
-streamlit.text('Kale')
-streamlit.text('Spinach')
-streamlit.text('Coleslaw')
+streamlit.header('Breakfast Favourties')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocade Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
